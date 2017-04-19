dd7e2a20c96d175e4d2813dba455e37f58c03758
meist3r-ed <gs4n.96@gmail.com>
2017-04-19 14:18:58 -0300

    Alignment & minor corrections :3

--- hash.asm
+++ hash.asm
@@ -19,70 +19,72 @@
 #		-1 - Fim da execução
 #
 
-hash_size:	.word	16 #sizeof hash table.
+.align 2
+hash_size:	.word	16 # sizeof hash table.
 
+.align 0
 Welcome_str:	.asciiz	"Welcome to Asm Hash Table!"
-Select_str:		.asciiz "\n\nPlease, choose an option"
+.align 0
+Select_str:	.asciiz "\n\nPlease, choose an option"
+.align 0
+insert_str:	.asciiz "\n1 - Inserção"
+.align 0
+remove_str:	.asciiz "\n2 - Remoção"
+.align 0
+search_str:	.asciiz "\n3 - Buscar Elemento"
+.align 0
+viewAll_str:	.asciiz "\n4 - Visualizar Tudo"
+.align 0
+exit_str:	.asciiz "\n-1 - Finalizar a execução.\n"
+.align 0
+invalidOp_str: 	.asciiz "\nCódigo Inválido\!\! \n\n\n"
 
-in4sert_str:		.asciiz "\n1 - Inserção"
-remove_str:		.asciiz "\n1 - Remoção"
-search_str:		.asciiz "\n1 - Buscar Elemento"
-viewAll_str:	.asciiz "\n1 - Visualizar Tudo"
-exit_str:			.asciiz "\n1 - Finalizar a execução.\n"
-invalidOp_str: .asciiz "\nCódigo Inválido\!\! \n\n\n"
 .text
 .globl main
 
 main:
 
 	HashMenu:
-		li $v0, 4 #Print_str
-		la $a0, Welcome_str # a0=&Welcome_str
+		li $v0, 4 		# Print_str
+		la $a0, Welcome_str 	# $a0 = &Welcome_str
 		syscall
 		
-		li $v0, 4 #Print_str
-		la $a0, Select_str # a0=&Select_str
+		la $a0, Select_str 	# $a0 = &Select_str
 		syscall
 		
-		
-		li $v0, 4 #Print_str
-		la $a0, insert_str # a0=&insert_str
+		la $a0, insert_str 	# $a0 = &insert_str
 		syscall
 		
-		li $v0, 4 #Print_str
-		la $a0, remove_str # a0=&remove_str
+		la $a0, remove_str 	# $a0 = &remove_str
 		syscall
 		
-		li $v0, 4 #Print_str
-		la $a0, search_str # a0=&search_str
+		la $a0, search_str 	# $a0 = &search_str
 		syscall
 		
-		li $v0, 4 #Print_str
-		la $a0, viewAll_str # a0=&viewAll_str
+		la $a0, viewAll_str 	# $a0 = &viewAll_str
 		syscall
 
-		li $v0, 4 #Print_str
-		la $a0, exit_str # a0=&Exit_str
+		la $a0, exit_str 	# $a0 = &Exit_str
 		syscall
 		
 		
-		li $v0, 5 #read_int
+		li $v0, 5 		# read_int
 		syscall
 		
 		
 		beq $v0, -1,Exit
-		beq $v0, 1, SelectLoop
-		beq $v0, 2, RemoveLoop
-		beq $v0, 3, SearchLoop
-		beq $v0, 4, ViewAllLoop
+		#beq $v0, 1, SelectLoop
+		#beq $v0, 2, RemoveLoop
+		#beq $v0, 3, SearchLoop
+		#beq $v0, 4, ViewAllLoop
 		
 		
-		li $v0, 4 #Print_str
-		la $a0, exit_str # a0=&invalidOp_str
+		li $v0, 4 		# Print_str
+		la $a0, exit_str 	# $a0 = &invalidOp_str
 		syscall
 		
 		j HashMenu
 	
 	Exit:
-		li $v0, 10 #exit
+		li $v0, 10 		# exit
 		syscall
